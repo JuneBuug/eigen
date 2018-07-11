5c84895e10cc8c338004b3bf14cca43a0b50c3db
Junebuug <junee613@gmail.com>
2018-07-12 05:37:09 +0900

    기록에서 emoji등 한글자만 입력가능하게 변경

--- eigen/commitCell.swift
+++ eigen/commitCell.swift
@@ -11,11 +11,31 @@ import UIKit
 class commitCell: UICollectionViewCell {
 
     @IBOutlet weak var background: UIView!
-    @IBOutlet weak var emoji: UILabel!
+    @IBOutlet weak var emoji: UITextField!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        let numberToolbar = UIToolbar(frame:CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        numberToolbar.barStyle = .default
+        numberToolbar.items = [
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.doneButtonAction))]
+        numberToolbar.sizeToFit()
+        emoji.inputAccessoryView = numberToolbar
     }
 
+    
+    @objc func doneButtonAction(){
+        checkMaxLength(textField:emoji, maxLength: 1)
+        self.emoji.resignFirstResponder()
+    }
+    
+    
+    func checkMaxLength(textField: UITextField!, maxLength: Int) {
+        if ((textField.text!.characters.count) > maxLength) {
+            textField.deleteBackward()
+        }
+    }
 }
